d51c8fe34a89289dbf5b0b298be37b38ce0bf74e
Kendu <kenduajayi@gmail.com>
2023-09-20 19:55:24 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,19 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """
+        Args:
+            cls: class type of an object
+        Return:
+            a dictionary of objects of one type of class if cls is not None;
+            else a dictionary of all objects
+        """
+        if cls:
+            new_dict = {}
+            for key, value in FileStorage.__objects.items():
+                if isinstance(value, cls):
+                    new_dict[key] = value
+            return new_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -50,6 +62,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
+        """
+        Deletes an object from a __objects if it is inside
+        Args:
+            obj: object to be deleted
+        """
         try:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             del FileStorage.__objects[key]
