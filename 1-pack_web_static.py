493bb975067e145e7a39e992ef0051f2ac4b0334
Kendu <kenduajayi@gmail.com>
2023-09-20 20:26:45 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ import os
 def do_pack():
     """ generates the .tgz archive from the contents
         of the web_static folder
-    """"
+    """
     # Gets the cuurent date and time
     now = datetime.now().strftime("%Y%m%d%H%M%S")
 
