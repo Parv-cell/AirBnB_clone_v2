8a116849f05043455c4201c498bbfa4d746bd564
Kendu <kenduajayi@gmail.com>
2023-09-20 20:21:19 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,24 +2,23 @@ from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
-	# Gets the cuurent date and time
-	now = datetime.now().strftime("%Y%m%d%H%M%S")
+    # Gets the cuurent date and time
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
 
-	# Creates the versions folder if it doesn't exist
-	if not os.path.exists("versions"):
-		os.makedirs("versions")
+    # Creates the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
 
-	# Sets the name of the archive file
-	archive_name = "web_static_" + now + ".tgz"
+    # Sets the name of the archive file
+    archive_name = "web_static_" + now + ".tgz"
 
-	# Creates the archive by compressing the web_static folder
-	result = local("tar -czvf versions/{} web_static".format(archive_name))
+    # Creates the archive by compressing the web_static folder
+    result = local("tar -czvf versions/{} web_static".format(archive_name))
 
-	# Checks if archive was created successfully
-	if result.succeeded:
-		# Returns the path of the created archive
-		return "version/" + archive_name
-	else:
-		# Returns None if the archive creation failed
-		return None
+    # Checks if the archive was created successfully
+    if result.succeeded:
+        return "version/" + archive_name
+    else:
+        return None
